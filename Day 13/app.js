5ecce1cd0aa39dcbed2a98412d6dfe7c3a7065f5
st1llwater <alokpunjofficial@gmail.com>
2021-07-06 17:28:12 +0530

    Day 15-17

--- Day 13/app.js
+++ Day 13/app.js
@@ -34,7 +34,7 @@ app.use(express.static("public"));
 
 app.get("/o", async (req, res) => {
   const apiResponse = await nodeFetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=Kolkata&appid=d0e499fb7d9968879c29a43c3689851d",
+    "https://api.openweathermap.org/data/2.5/weather?q=Mussoorie&appid=d0e499fb7d9968879c29a43c3689851d",
     {
       method: "GET",
     }
