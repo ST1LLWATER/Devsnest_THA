96ac600a849378e7b66f1c11f21b04db5f28eebb
st1llwater <alokpunjofficial@gmail.com>
2021-07-07 18:45:48 +0530

    Chess Board Completed

---
+++ Day 18/chess_board/src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import React from "react";
+
+function Box(props) {
+  console.log(props.alter, props.i);
+  if (props.alter) {
+    return (
+      <div
+        style={{ backgroundColor: props.i % 2 === 0 ? "black" : "white" }}
+        className="box"
+      ></div>
+    );
+  } else {
+    return (
+      <div
+        style={{ backgroundColor: props.i % 2 === 0 ? "white" : "black" }}
+        className="box"
+      ></div>
+    );
+  }
+}
+
+function App() {
+  let alter = true;
+  return (
+    <>
+      <div id="wrapper">
+        {[...Array(64)].map((box, i) => {
+          if (i % 8 === 0 && i !== 0) {
+            alter = !alter;
+          }
+          return <Box i={i} alter={alter} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
