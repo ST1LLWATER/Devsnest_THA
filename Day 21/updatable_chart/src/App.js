7e8561c153b73fb67b53e0824a0e09c24764af40
st1llwater <alokpunjofficial@gmail.com>
2021-07-12 21:45:56 +0530

    Completed Day 21

--- Day 21/updatable_chart/src/App.js
+++ Day 21/updatable_chart/src/App.js
@@ -1,28 +1,69 @@
 import { useState } from "react";
 import "./App.css";
-import Card from "./Assets/Card";
+import Card from "./components/Card";
 
 function App() {
-  const [title,setTitle]=useState("");
-  const [calorie,setCalorie]=useState("");
-  function handleClick(e){
+  const [title, setTitle] = useState("");
+  const [calorie, setCalorie] = useState("");
+  const [cards, setCards] = useState([]);
+
+  function handleClick(e) {
     e.preventDefault();
-    // addItem({title,calorie});
+
+    cards.push({ title, calorie });
+    setCards(cards);
+
     setTitle("");
     setCalorie("");
   }
+
+  function handleDelete(index) {
+    setCards(cards.filter((_, i) => i !== index));
+  }
+
+
+
+  // function handleEdit()
+
   return (
     <>
-    <form className="form" onSubmit={handleClick}>
-    <div className="wrapper">
-    <div className="inputs">
-      <input type="text" name="name" id="name" placeholder="Item Name" value={title} onChange={e=> setTitle(e.target.value)} required />
-      <input type="number" name="calorie" id="calorie" placeholder="Calorie Amount" value={calorie} onChange={e=> setCalorie(e.target.value)} required /></div>
-      <button type="submit" className="submit">ADD ITEM</button></div>
-    </form>
-    <div className="items">
-     <Card title={title} calorie={calorie}/>
-     </div>
+      <form className="form" onSubmit={handleClick}>
+        <div className="wrapper">
+          <div className="inputs">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Item Name"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+            <input
+              type="number"
+              name="calorie"
+              id="calorie"
+              placeholder="Calorie Amount"
+              value={calorie}
+              onChange={(e) => setCalorie(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="submit">
+            ADD ITEM
+          </button>
+        </div>
+      </form>
+      <div className="items">
+        {cards.map((data, i) => (
+          <Card
+            key={i}
+            title={data.title}
+            calorie={data.calorie}
+            handleDelete={() => handleDelete(i)}
+          />
+        ))}
+      </div>
     </>
   );
 }
